eabb8f214b505ab00c716285792fabac1d78a9e3
EeveeRap <volvirapaport@gmail.com>
2023-06-01 11:37:44 -0400

    Completed HW 51

---
+++ html/51/hw51.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Headers;
+using System.Xml.Serialization;
+
+namespace Practice1
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = {"red", "green", "blue" };
+            string[] patterns = { "striped", "checked", "plain" };
+            
+            
+
+            for(int i = 0; i < colors.Length; i++)
+            {
+                
+                for (int j = 0; j < patterns.Length; j++)
+                {
+                    shirt s1 = new shirt(colors[i], patterns[j]);                    
+                    s1.GetShirt();
+                }
+                
+            }                      
+        }
+        public class shirt
+        {
+            public string Color { get; set; }
+            public string Pattern { get; set; }
+            public shirt(string color, string pattern)
+            {
+                Color = color;
+                Pattern = pattern;
+            }
+            public void GetShirt()
+            {
+                Console.WriteLine($"Color: {Color}, Pattern {Pattern}");
+            }
+            
+        }
+        
+            
+
+    }
+}
