734a7e5425190fcb475782cf3be891a83916ff66
EeveeRap <volvirapaport@gmail.com>
2023-10-20 17:11:27 -0400

    Completed HW 78

---
+++ js/78/hw.js
@@ -0,0 +1,35 @@
+/* global $*/
+(function () {
+    "use strict";
+
+    const file = $("#file");
+    const loadButton = $("#loadButton");
+    const output = $("#output");
+    const loadingImage = new Image();
+    loadingImage.src = 'loading.gif';
+
+    loadButton.click(async () => {
+        const inputValue = file.val();
+        output.html(loadingImage);
+
+        setTimeout(async () => {
+            try {
+                const response = await fetch(inputValue);
+
+                if (response.status >= 400) {
+                    throw new Error(`${response.status} ${response.statusText}`);
+                }
+
+                const data = await response.text();
+                setTimeout(3000);
+                // console.log(data, typeof data, response.status);
+                output.html(`The output is as follows: \n ${data}`)
+            } catch (e) {
+                const errorMessage = `Oops, an error occurred: ${e.message}`;
+                output.html(errorMessage);
+            }
+        }, 3000);
+
+    });
+
+}());
