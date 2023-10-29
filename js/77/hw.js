2f55686ec6395493e349b2d18837c821734d19eb
EeveeRap <volvirapaport@gmail.com>
2023-10-29 14:01:58 -0400

    Fixed HW 77

--- js/77/hw.js
+++ js/77/hw.js
@@ -1,11 +1,25 @@
 /*global $*/
-(function() {
- "use strict";
- 
-const oddButton = $("#oddButton");
-const evenButton = $("#evenButton");
+(function () {
+    "use strict";
 
+    const oddButton = $("#oddButton");
+    const evenButton = $("#evenButton");
 
+    oddButton.click(() => {
+        $("p:nth-child(odd) ")
+            .css("color", "yellow");
+        $("p:nth-child(even) ")
+            .css("color", "");
+
+    });
+
+    evenButton.click(() => {
+        $("p:nth-child(even) ")
+            .css("color", "yellow");
+        $("p:nth-child(odd) ")
+            .css("color", "");
+
+    });
 
 
 
